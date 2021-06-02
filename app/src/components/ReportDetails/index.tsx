4d4661f0360cd437e9f5400b2d195b87d0fff04e
David Caro <david@dcaro.es>
2021-06-02 14:32:34 +0200

    Improved a bit the landing page.
    This included refactoring the inline styles of ReportDetails.
    
    Change-Id: Ic513ef6af1c93f8d36ed8422b0886486e1da65a9
    Signed-off-by: David Caro <david@dcaro.es>

--- app/src/components/ReportDetails/index.tsx
+++ app/src/components/ReportDetails/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Report } from '../../types';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    selectReport: {
+        position: "absolute",
+        left: "500px",
+        fontSize: "100px",
+    },
+    iframe: {
+        height: "100%",
+        width: "50%",
+        flex: "auto",
+    }
+}))
 
 export function ReportDetails(props: { report: Report | null | undefined }): JSX.Element {
+    const classes = useStyles();
+
     if (props.report == undefined || props.report === null) {
-        return <div>Select a report.</div>;
+        return <div className={classes.selectReport}>Select a report.</div>;
     }
-    return <iframe style={{height: "100%", width: "50%", flex: "auto"}} src={props.report.url}/>
+    return <iframe className={classes.iframe} src={props.report.url}/>
 }
